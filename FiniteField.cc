6b4123d3699a264535cce8f5ddd71924b70f8c25
Alif-01 <alif01@163.com>
2022-05-25 15:41:07 +0800

    Minor changes to FiniteField

---
+++ FiniteField.cc
@@ -0,0 +1,129 @@
+#include "FiniteField.h"
+
+// Integer
+
+Integer operator+(const Integer &a, const Integer &b) {
+  Integer c;
+  mpz_add(c.v_, a.v_, b.v_);
+  return c;
+}
+
+Integer operator-(const Integer &a, const Integer &b) {
+  Integer c;
+  mpz_sub(c.v_, a.v_, b.v_);
+  return c;
+}
+
+Integer operator*(const Integer &a, const Integer &b) {
+  Integer c;
+  mpz_mul(c.v_, a.v_, b.v_);
+  return c;
+}
+
+Integer operator*(const Integer &a, const unsigned int &b) {
+  Integer c;
+  mpz_mul_ui(c.v_, a.v_, b);
+  return c;
+}
+
+Integer operator%(const Integer &a, const Integer &b) {
+  Integer r;
+  mpz_mod(r.v_, a.v_, b.v_);
+  return r;
+}
+
+Integer inv(const Integer &a, const Integer &b) {
+  Integer res;
+  mpz_invert(res.v_, a.v_, b.v_);
+  return res;
+}
+
+int cmp(const Integer &a, const Integer &b) {
+  return mpz_cmp(a.v_, b.v_);
+}
+
+// Fp
+
+Fp operator-(const Fp &a) {
+  if (mpz_sgn(a.v_.v_) == 0) {
+    return a;
+  }
+  return Fp{a.p_, a.p_} - a;
+}
+
+Fp operator+(const Fp &a, const Fp &b) {
+#ifdef DEBUG
+  assert(cmp(a.p_, b.p_) == 0);
+#endif
+  Integer c = a.v_ + b.v_;
+  if (cmp(c, a.p_) >= 0) {
+    c = c - a.p_;
+  }
+  return Fp{c, a.p_};
+}
+
+Fp operator-(const Fp &a, const Fp &b) {
+#ifdef DEBUG
+  assert(cmp(a.p_, b.p_) == 0);
+#endif
+  Integer c = a.v_;
+  if (cmp(c, b.v_) < 0) {
+    c = c + a.p_;
+  }
+  c = c - b.v_;
+  return Fp{c, a.p_};
+}
+
+Fp operator*(const Fp &a, const Fp &b) {
+#ifdef DEBUG
+  assert(cmp(a.p_, b.p_) == 0);
+#endif
+  Integer c = a.v_ * b.v_ % a.p_;
+  return Fp{c, a.p_};
+}
+
+Fp operator*(const Fp &a, const unsigned &b) {
+  Integer c = a.v_ * b % a.p_;
+  return Fp{c, a.p_};
+}
+
+Fp operator/(const Fp &a, const Fp &b) {
+#ifdef DEBUG
+  assert(cmp(a.p_, b.p_) == 0);
+#endif
+  Integer c = a.v_ * b.inv().v_ % a.p_;
+  return Fp{c, a.p_};
+}
+
+bool operator==(const Fp &a, const Fp &b) {
+  return cmp(a.p_, b.p_) == 0 && cmp(a.v_, b.v_) == 0;
+}
+
+// Fp2
+
+Fp2 operator+(const Fp2 &a, const Fp2 &b) {
+  return Fp2{a.x_ + b.x_, a.y_ + b.y_};
+}
+
+Fp2 operator-(const Fp2 &a, const Fp2 &b) {
+  return Fp2{a.x_ - b.x_, a.y_ - b.y_};
+}
+
+Fp2 operator*(const Fp2 &a, const Fp2 &b) {
+  Fp c2 = a.x_ * b.x_;
+  Fp c0 = a.y_ * b.y_;
+  Fp c1 = (a.x_ + a.y_) * (b.x_ + b.y_) - c2 - c0;
+  return Fp2{c2 - c0, c1};
+} // 3M
+
+Fp2 operator*(const Fp2 &a, const unsigned int &b) {
+  return Fp2{a.x_ * b, a.y_ * b};
+}
+
+Fp2 operator/(const Fp2 &a, const Fp2 &b) {
+  return a * b.inv();
+}
+
+bool operator==(const Fp2 &a, const Fp2 &b) {
+  return a.x_ == b.x_ && a.y_ == b.y_;
+}
--- FiniteField.h
+++ FiniteField.h
@@ -1,5 +1,4 @@
 #pragma once
-#define DEBUG
 
 #include "gmp.h"
 #include <utility>
@@ -54,45 +53,17 @@ class Integer {
   mpz_t v_;
 };
 
-Integer operator+(const Integer &a, const Integer &b) {
-  Integer c;
-  mpz_add(c.v_, a.v_, b.v_);
-  return c;
-}
-Integer operator-(const Integer &a, const Integer &b) {
-  Integer c;
-  mpz_sub(c.v_, a.v_, b.v_);
-  return c;
-}
-Integer operator*(const Integer &a, const Integer &b) {
-  Integer c;
-  mpz_mul(c.v_, a.v_, b.v_);
-  return c;
-}
-Integer operator*(const Integer &a, const unsigned int &b) {
-  Integer c;
-  mpz_mul_ui(c.v_, a.v_, b);
-  return c;
-}
-Integer operator%(const Integer &a, const Integer &b) {
-  Integer r;
-  mpz_mod(r.v_, a.v_, b.v_);
-  return r;
-}
-Integer inv(const Integer &a, const Integer &b) {
-  Integer res;
-  mpz_invert(res.v_, a.v_, b.v_);
-  return res;
-}
-int cmp(const Integer &a, const Integer &b) {
-  return mpz_cmp(a.v_, b.v_);
-}
+Integer operator+(const Integer &a, const Integer &b);
+Integer operator-(const Integer &a, const Integer &b);
+Integer operator*(const Integer &a, const Integer &b);
+Integer operator*(const Integer &a, const unsigned int &b);
+Integer operator%(const Integer &a, const Integer &b);
+Integer inv(const Integer &a, const Integer &b);
+int cmp(const Integer &a, const Integer &b);
 
 class Fp {
  public:
-  Fp(Integer x, Integer p): v_{std::move(x)}, p_{std::move(p)} {}
-  Fp(const Fp &fp): v_{fp.v_}, p_{fp.p_} {}
-  Fp(Fp &&fp): v_{fp.v_}, p_{fp.p_} {}
+  Fp(Integer v, Integer p): v_{v}, p_{p} {}
   ~Fp() = default;
 
   friend Fp operator-(const Fp &a);
@@ -117,65 +88,28 @@ class Fp {
     return v_.to_string() + " ( mod " + p_.to_string() + " )";
   }
 
+  bool zero() const{
+    return cmp(v_, 0) == 0;
+  }
+
  private:
   Integer v_;
   Integer p_;
 };
 
-Fp operator-(const Fp &a) {
-  if (mpz_sgn(a.v_.v_) == 0) {
-    return a;
-  }
-  return Fp{a.p_, a.p_} - a;
-}
-Fp operator+(const Fp &a, const Fp &b) {
-#ifdef DEBUG
-  assert(cmp(a.p_, b.p_) == 0);
-#endif
-  Integer c = a.v_ + b.v_;
-  if (cmp(c, a.p_) >= 0) {
-    c = c - a.p_;
-  }
-  return Fp{c, a.p_};
-}
-Fp operator-(const Fp &a, const Fp &b) {
-#ifdef DEBUG
-  assert(cmp(a.p_, b.p_) == 0);
-#endif
-  Integer c = a.v_;
-  if (cmp(c, b.v_) < 0) {
-    c = c + a.p_;
-  }
-  c = c - b.v_;
-  return Fp{c, a.p_};
-}
-Fp operator*(const Fp &a, const Fp &b) {
-#ifdef DEBUG
-  assert(cmp(a.p_, b.p_) == 0);
-#endif
-  Integer c = a.v_ * b.v_ % a.p_;
-  return Fp{c, a.p_};
-}
-Fp operator*(const Fp &a, const unsigned &b) {
-  Integer c = a.v_ * b % a.p_;
-  return Fp{c, a.p_};
-}
-Fp operator/(const Fp &a, const Fp &b) {
-#ifdef DEBUG
-  assert(cmp(a.p_, b.p_) == 0);
-#endif
-  Integer c = a.v_ * b.inv().v_ % a.p_;
-  return Fp{c, a.p_};
-}
-bool operator==(const Fp &a, const Fp &b) {
-  return cmp(a.p_, b.p_) == 0 && cmp(a.v_, b.v_) == 0;
-}
+Fp operator-(const Fp &a);
+Fp operator+(const Fp &a, const Fp &b);
+Fp operator-(const Fp &a, const Fp &b);
+Fp operator*(const Fp &a, const Fp &b);
+Fp operator*(const Fp &a, const unsigned int &b);
+Fp operator/(const Fp &a, const Fp &b);
+bool operator==(const Fp &a, const Fp &b);
 
 // scalar in F_{p^2}
 // x + iy
 class Fp2 {
  public:
-  Fp2(Integer x, Integer y, Integer p) : x_{std::move(x), p}, y_{std::move(y), p} {}
+  Fp2(Integer x, Integer y, Integer p) : x_{x, p}, y_{y, p} {}
   Fp2(Fp x, Fp y) : x_{x}, y_{y} {
 #ifdef DEBUG
     assert(cmp(x.p_, y.p_) == 0);
@@ -196,6 +130,7 @@ class Fp2 {
     Fp y = x_ * y_ * 2;
     return Fp2{x, y};
   }
+
   // 1I + 2S + 2M
   Fp2 inv() const {
     Fp denominator = x_ * x_ + y_ * y_;
@@ -204,14 +139,21 @@ class Fp2 {
     Fp y = -y_ * denominator_inv;
     return Fp2{x, y};
   }
+
   Fp get_x() const {
     return x_;
   }
+
   Fp get_y() const {
     return y_;
   }
+
   std::string to_string() const {
     return x_.get_v().to_string() + " + " + y_.get_v().to_string() + " i ( mod " + x_.get_p().to_string() + " )";
+  }
+
+  bool zero() const {
+    return x_.zero() && y_.zero();
   }
 
  private:
@@ -219,25 +161,9 @@ class Fp2 {
   Fp y_;
 };
 
-Fp2 operator+(const Fp2 &a, const Fp2 &b) {
-  return Fp2{a.x_ + b.x_, a.y_ + b.y_};
-}
-Fp2 operator-(const Fp2 &a, const Fp2 &b) {
-  return Fp2{a.x_ - b.x_, a.y_ - b.y_};
-}
-// 3M
-Fp2 operator*(const Fp2 &a, const Fp2 &b) {
-  Fp c2 = a.x_ * b.x_;
-  Fp c0 = a.y_ * b.y_;
-  Fp c1 = (a.x_ + a.y_) * (b.x_ + b.y_) - c2 - c0;
-  return Fp2{c2 - c0, c1};
-}
-Fp2 operator*(const Fp2 &a, const unsigned int &b) {
-  return Fp2{a.x_ * b, a.y_ * b};
-}
-Fp2 operator/(const Fp2 &a, const Fp2 &b) {
-  return a * b.inv();
-}
-bool operator==(const Fp2 &a, const Fp2 &b) {
-  return a.x_ == b.x_ && a.y_ == b.y_;
-}
+Fp2 operator+(const Fp2 &a, const Fp2 &b);
+Fp2 operator-(const Fp2 &a, const Fp2 &b);
+Fp2 operator*(const Fp2 &a, const Fp2 &b);
+Fp2 operator*(const Fp2 &a, const unsigned int &b);
+Fp2 operator/(const Fp2 &a, const Fp2 &b);
+bool operator==(const Fp2 &a, const Fp2 &b);
